83712b8bd034e5b1208fafec4cab9e7e8dc314d4
rafaeljosecomputacao <132665090+rafaeljosecomputacao@users.noreply.github.com>
2023-06-03 06:51:59 -0300

    construtores-this-sobrecarga-encapsulamento

---
+++ Exercicio7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria contaBancaria;
+
+            Console.Write("Entre com o numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Havera deposito inicial (s/n)? ");
+            char resposta = char.Parse(Console.ReadLine());
+            if (resposta ==  's' || resposta == 'S')
+            {
+                Console.Write("Entre com o valor de deposito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                contaBancaria = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else
+            {
+                contaBancaria = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(contaBancaria);
+
+            Console.WriteLine();
+            Console.Write("Entre com um valor para deposito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            contaBancaria.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(contaBancaria);
+
+            Console.WriteLine();
+            Console.Write("Entre com um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            contaBancaria.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(contaBancaria);
+        }
+    }
+}

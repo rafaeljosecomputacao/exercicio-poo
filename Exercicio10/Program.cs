07b08bbaccde82df97e74aef44394de4a08e88b1
rafaeljosecomputacao <132665090+rafaeljosecomputacao@users.noreply.github.com>
2023-06-05 06:45:47 -0300

    memoria-arrays-listas

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Exercicio10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] auxiliar = Console.ReadLine().Split(' ');
+            int m = int.Parse(auxiliar[0]);
+            int n = int.Parse(auxiliar[1]);
+            int[,] matriz = new int[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                auxiliar = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    matriz[i, j] = int.Parse(auxiliar[j]);
+                }
+            }
+
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matriz[i, j] == x)
+                    {
+                        Console.WriteLine("Posicao " + i + "," + j + ":");
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Esquerda: " + matriz[i, j - 1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Acima: " + matriz[i - 1, j]);
+                        }
+                        if (j < n - 1)
+                        {
+                            Console.WriteLine("Direita: " + matriz[i, j + 1]);
+                        }
+                        if (i < m - 1)
+                        {
+                            Console.WriteLine("Abaixo: " + matriz[i + 1, j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Exercicio9/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Exercicio9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Funcionario> listaFuncionario = new List<Funcionario>();
+
+            Console.Write("Quantos funcionarios vao ser registrados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Funcionario #" + i + ":");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Salario: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                listaFuncionario.Add(new Funcionario(id, nome, salario));
+                Console.WriteLine();
+            }
+
+            Console.Write("Entre com o id do funcionario que tera o salario aumentado: ");
+            int buscaId = int.Parse(Console.ReadLine());
+
+            Funcionario funcionario = listaFuncionario.Find(x => x.Id == buscaId);
+            if (funcionario != null)
+            {
+                Console.Write("Entre com a porcentagem: ");
+                double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                funcionario.AumentarSalario(porcentagem);
+            }
+            else
+            {
+                Console.WriteLine("Este id nao existe!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Lista atualizada de funcionarios:");
+            foreach (Funcionario objeto in listaFuncionario)
+            {
+                Console.WriteLine(objeto);
+            }
+        }
+    }
+}
